045582c7df15ce3ebd5b438348ef19f941a373ba
watanabe1095 <wassan5656@gmail.com>
2025-03-07 15:24:46 +0900

    Update index.py

--- pipeline_settings/index.py
+++ pipeline_settings/index.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     print(event)
     return {
         'statusCode': 200,
-        'body': json.dumps('hamburger steak Lambda!')
+        'body': json.dumps('Hello hamburger steak Lambda!')
     }
